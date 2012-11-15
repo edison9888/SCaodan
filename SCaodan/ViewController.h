0e545c4fdee6f191270ec1ef56af3cb77d56acda
sunjiangting <jiangting.sun@renren-inc.com>
2012-11-15 15:45:01 +0800

    Initial Commit

---
+++ SCaodan/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SCaodan
+//
+//  Created by SunJiangting on 12-11-15.
+//  Copyright (c) 2012年 sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
